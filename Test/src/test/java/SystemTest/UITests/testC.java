971e70c5eb1df4bba15abc2444b9ff967fde2f55
Sujay Ghorpade <sujay.ghorpade@siemens-healthineers.com>
2019-06-11 14:41:09 +0530

    testCom

---
+++ Test/src/test/java/SystemTest/UITests/testC.java
@@ -0,0 +1,25 @@
+package SystemTest.UITests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+import java.util.List;
+
+public class testC {
+    @Test
+    void main(){
+        System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        List<WebElement> list = driver.findElements(By.tagName("*"));
+        for(WebElement e : list){
+            System.out.println(e.getText());
+        }
+        driver.quit();
+    }
+}
