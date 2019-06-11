07380cff407fcd09bc5c7ccc6b2b7f7e53478c8c
Sujay Ghorpade <sujay.ghorpade@siemens-healthineers.com>
2019-06-11 16:03:15 +0530

    BasicFrame

--- Test/src/test/java/SystemTest/UITests/testC.java
+++ Test/src/test/java/SystemTest/UITests/testC.java
@@ -5,18 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-
 import java.util.List;
 
 public class testC {
     @Test
-    void main(){
+    public void main(){
         System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
 
-        List<WebElement> list = driver.findElements(By.tagName("*"));
+        List<WebElement> list = driver.findElements(By.tagName("a"));
         for(WebElement e : list){
             System.out.println(e.getText());
         }
